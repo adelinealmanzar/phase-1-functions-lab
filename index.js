323401df3668bcabc294cfeef0158d4902c30afb
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-01-11 15:02:36 -0800

    commit tree 7d1d8711df0671db891c1b62fa762a4b51b152a5

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(value) {
+    return Math.abs(42 - value);
+}
+
+function distanceFromHqInFeet(value) {
+    return distanceFromHqInBlocks(value)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const dist = distanceTravelledInFeet(start, destination);
+    if (dist <= 400) {
+        return 0;
+    } else if (dist > 400 && dist < 2000) {
+        return (dist - 400) * (2/100);
+    } else if (dist > 2000 && dist < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
